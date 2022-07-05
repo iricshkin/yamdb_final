998d5864ad6c626961a422fb9e869f159e882e26
Ирина Фок <fokira@mail.ru>
2022-07-06 00:12:38 +0300

    edit static_root

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [BASE_DIR + '/api_yamdb/static/']
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
