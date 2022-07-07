2a9b43b7c481e93b47f79a18a817bbb1dcbb9e8d
Ирина Фок <fokira@mail.ru>
2022-07-07 22:00:45 +0300

    edit entrypoint

--- api_yamdb/entrypoint.sh
+++ api_yamdb/entrypoint.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
-if [ -z "$1" ]
-  then
-    echo "No argument supplied"
-    exit 1
-elif [ $1 = "migrate" ]
-  then
-      python manage.py makemigrations
-      python manage.py migrate
-elif [ $1 = "pre-run" ]
-  then
-      python manage.py collectstatic --noinput
-elif [ $1 = "run" ]
-  then
-    exec $(which gunicorn) api_yamdb.wsgi:application --bind=0:8080
-    exit $?
-elif [ $1 = "all" ]
-  then
-      python manage.py makemigrations
-      python manage.py migrate
-    exec $(which gunicorn) api_yamdb.wsgi:application --bind=0:8080
-    exit $?
-else
-  echo "Invalid argument"
-  exit 1
-fi
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+exec "$@"
